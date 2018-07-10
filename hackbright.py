24d393fb8e421059ca9e3c26418a134bcd6d6c10
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-10 12:44:44 -0700

    completed all functions

--- hackbright.py
+++ hackbright.py
@@ -42,22 +42,59 @@ def make_new_student(first_name, last_name, github):
     Given a first name, last name, and GitHub account, add student to the
     database and print a confirmation message.
     """
-    pass
+
+    QUERY = """
+        INSERT INTO students (first_name, last_name, github)
+        VALUES (:first_name, :last_name, :github)
+        """
+
+    db.session.execute(QUERY, {'first_name': first_name,
+                               'last_name': last_name,
+                               'github': github})
+    db.session.commit()
+    print(f"Successfully added student: {first_name} {last_name}")
 
 
 def get_project_by_title(title):
     """Given a project title, print information about the project."""
-    pass
+
+    QUERY = """
+        SELECT title, description FROM projects WHERE title = :title
+        """
+
+    db_cursor = db.session.execute(QUERY, {'title': title})
+    row = db_cursor.fetchone()
+
+    print(f"Description of {row[0]} is: {row[1]}.")
 
 
 def get_grade_by_github_title(github, title):
     """Print grade student received for a project."""
-    pass
+
+    QUERY = """
+        SELECT grade FROM grades
+        WHERE student_github = :github AND project_title = :title
+        """
+
+    db_cursor = db.session.execute(QUERY, {'github': github, 'title': title})
+    row = db_cursor.fetchone()
+
+    print(f'The grade for {github} for the {title} project is {row[0]}.')
 
 
 def assign_grade(github, title, grade):
     """Assign a student a grade on an assignment and print a confirmation."""
-    pass
+    QUERY = """
+        INSERT INTO grades (student_github, project_title, grade)
+        VALUES (:github, :title, :grade)
+        """
+
+    db.session.execute(QUERY, {'github': github,
+                               'title': title,
+                               'grade': grade})
+
+    db.session.commit()
+    print(f'{github} earned {grade} points for {title}.')
 
 
 def handle_input():
@@ -79,6 +116,18 @@ def handle_input():
             github = args[0]
             get_student_by_github(github)
 
+        elif command == "title":
+            title = args[0]
+            get_project_by_title(title)
+
+        elif command == "get_grade":
+            github, title = args
+            get_grade_by_github_title(github, title)
+
+        elif command == "assign_grade":
+            github, title, grade = args
+            assign_grade(github, title, grade)
+
         elif command == "new_student":
             first_name, last_name, github = args  # unpack!
             make_new_student(first_name, last_name, github)
@@ -91,7 +140,7 @@ def handle_input():
 if __name__ == "__main__":
     connect_to_db(app)
 
-    # handle_input()
+    handle_input()
 
     # To be tidy, we close our database connection -- though,
     # since this is where our program ends, we'd quit anyway.
